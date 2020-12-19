f1e9275729e293e7e340ec42e7dbbce10f2156cb
tomash02 <70844011+tomash02@users.noreply.github.com>
2020-12-19 09:33:22 +0100

    Update obstacles.py

--- obstacles.py
+++ obstacles.py
@@ -9,5 +9,5 @@ class Obstacles:
         self.sprite = sprite
 
     def draw(self, win):
-        pygame.draw.rect(win, (200, 100, 21), self.cords, 2)
+        # pygame.draw.rect(win, (200, 100, 21), self.cords, 2)
         win.blit(self.sprite, (self.cords[0], self.cords[1]))
